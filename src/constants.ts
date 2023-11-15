a9c0b81049c0621ccfce33588f9d28d33ff461e6
pavel <kovrizhkopavel@gmail.com>
2023-11-16 01:20:55 +0300

    feat: make cli server command

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,11 @@ export const ExitCode = {
 export const USER_ARGV_INDEX = 2;
 
 export const DEFAULT_COMMAND = '--help';
+
+export const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+} as const;
