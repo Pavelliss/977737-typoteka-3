a94d5dedfb1dac995b582b2bd763cd1e8edf94d8
pavel <kovrizhkopavel@gmail.com>
2023-11-22 02:41:56 +0300

    refactor: remove comments

--- src/express/express.ts
+++ src/express/express.ts
@@ -20,14 +20,3 @@ app.set('views', path.resolve(__dirname, TEMPLATES_DIR));
 app.set('view engine', 'pug');
 
 app.listen(DEFAULT_PORT);
-
-/*
-post-detail.pug — страница публикации (post-user.html);
-articles-by-category.pug — публикации в определённой категории (publications-by-category.html);
-comments.pug — список комментариев к публикациям (admin-comments.html);
-my.pug — мои публикации (admin-publications.html);
-post.pug — страница создания и редактирование публикации (admin-add-new-post-empty.html);
-all-categories.pug — страница со списком всех категорий (admin-categories.html)
-500.pug — страница для отображения ошибок 5** (500.html);
-404.pug — страница для отображения ошибки 404 (404.html). Внутри директории templates вы можете выстраивать произвольную иерархию директорий. Например, шаблоны для страниц с ошибками могут размещаться в директории ./express/templates/errors и так далее. Для отдельных повторяющихся блоков вы также можете завести отдельную директорию внутри templates.
-* */
