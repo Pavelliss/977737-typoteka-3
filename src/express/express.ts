6e092912ff37b627b813cf60b0181728e1cde42b
pavel <kovrizhkopavel@gmail.com>
2023-11-22 00:48:03 +0300

    feat: add routers to express

--- src/express/express.ts
+++ src/express/express.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import userRouter from './routes/user';
+import articleRouter from './routes/articles';
+import mainRoutes from './routes/main';
 
 const DEFAULT_PORT = 8080;
 
 const app = express();
+
+app.use('/', mainRoutes);
+app.use('/user', userRouter);
+app.use('article', articleRouter);
 
 app.listen(DEFAULT_PORT);
