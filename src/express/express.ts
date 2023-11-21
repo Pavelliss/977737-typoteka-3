d2fdc0f15b75120e7ffaa1e6ae7acf16cd2a440f
pavel <kovrizhkopavel@gmail.com>
2023-11-22 01:07:15 +0300

    feat: configurate express for pug

--- src/express/express.ts
+++ src/express/express.ts
@@ -2,13 +2,21 @@ import express from 'express';
 import userRouter from './routes/user';
 import articleRouter from './routes/articles';
 import mainRoutes from './routes/main';
+import * as path from 'path';
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = 'public';
+const TEMPLATES_DIR = 'templates';
 
 const app = express();
 
 app.use('/', mainRoutes);
 app.use('/user', userRouter);
 app.use('article', articleRouter);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set('views', path.resolve(__dirname, TEMPLATES_DIR));
+app.set('view engine', 'pug');
 
 app.listen(DEFAULT_PORT);
