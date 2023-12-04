bc70a63a5986da793dfbe8f5e276d356ded92b81
pavel <kovrizhkopavel@gmail.com>
2023-12-04 22:16:27 +0300

    feat: add search to route

--- src/service/api/index.ts
+++ src/service/api/index.ts
@@ -4,11 +4,14 @@ import category from './category';
 import Category from '../data-service/category';
 import article from './article';
 import Article from '../data-service/article';
+import search from './search';
+import Search from '../data-service/search';
 
 const app = Router();
 
 getMockData().then((articles) => {
   category(app, new Category(articles));
   article(app, new Article(articles));
+  search(app, new Search(articles));
 });
 export default app;
