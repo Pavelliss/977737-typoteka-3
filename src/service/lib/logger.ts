ab1921eeeeed7b7c4efdb9bd1dda5b4991bd4d73
pavel <kovrizhkopavel@gmail.com>
2023-12-30 17:34:25 +0300

    feat: add config to logger

--- src/service/lib/logger.ts
+++ src/service/lib/logger.ts
@@ -3,6 +3,9 @@ import pino from 'pino';
 export const logger = pino({
   name: `base-logger`,
   level: `info`,
+  transport: {
+    target: 'pino-pretty',
+  },
 });
 
 export function getLogger(options = {}) {
