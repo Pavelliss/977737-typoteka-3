3072a86fc04ef4a8e0ec714565df6fdb0fce4dad
pavel <kovrizhkopavel@gmail.com>
2023-12-30 18:05:57 +0300

    feat: add Env constants

--- src/constants.ts
+++ src/constants.ts
@@ -20,3 +20,8 @@ export const HttpCode = {
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
 } as const;
+
+export const Env = {
+  DEVELOPMENT: `development`,
+  PRODUCTION: `production`,
+} as const;
