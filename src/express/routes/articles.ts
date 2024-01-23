de9934274de7ce8eb0a3d1ec1174012c32a090d0
pavel <kovrizhkopavel@gmail.com>
2024-01-23 21:15:57 +0300

    feat: change scripts

--- src/express/routes/articles.ts
+++ src/express/routes/articles.ts
@@ -2,7 +2,9 @@ import { Router } from 'express';
 
 const articleRouter = Router();
 
-articleRouter.get('/category/:id', (_, res) => res.render('articles/articles-by-category'));
+articleRouter.get('/category/:id', (_, res) =>
+  res.render('articles/articles-by-category'),
+);
 articleRouter.get('/add', (_, res) => res.render('articles/post'));
 articleRouter.get('/edit/:id', (_, res) => res.render('articles/post'));
 articleRouter.get('/:id', (_, res) => res.render('articles/post-detail'));
--- src/express/routes/main.ts
+++ src/express/routes/main.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import { defaultAPI } from '../api';
 
 const mainRoutes = Router();
+const api = defaultAPI;
 
-mainRoutes.get('/', (_, res) => res.render('main'));
+mainRoutes.get('/', async (_, res) => {
+  const articles = await api.getArticles();
+  res.render('main', { articles });
+});
 mainRoutes.get('/register', (_, res) => res.render('sign-up'));
 mainRoutes.get('/login', (_, res) => res.render('login'));
 mainRoutes.get('/search', (_, res) => res.render('search'));
